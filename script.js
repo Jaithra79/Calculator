034bdabf6492b351577695c36db8bbd070f23176
jaithra <jjaithra79@gmail.com>
2024-12-20 22:55:55 +0530

    on process

---
+++ script.js
@@ -0,0 +1,4 @@
+const currDisplay = document.querySelector(".curr-display");
+const prevDisplay = document.querySelector(".prev-display");
+const numbers = document.querySelectorAll(".number");
+const operands = document.querySelectorAll(".operation");
